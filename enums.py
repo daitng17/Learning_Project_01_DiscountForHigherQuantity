5e4a0cd3493f2aa2578c223dd2a7495c9146f934
Thanh D T Nguyen <36881258+thanhdtnguyen@users.noreply.github.com>
2021-01-25 17:43:29 +0900

    Add files via upload

---
+++ enums.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class Fruit(Enum):
+    APPLE = 1
+    BANANA = 2
+    ORANGE = 3
+    TOMATO = 4
+
+
+def main():
+    pass
+    print(Fruit.APPLE)
+    print(type(Fruit.APPLE))
+    print(repr(Fruit.APPLE))
+
+if __name__ == "__main__":
+    main()
