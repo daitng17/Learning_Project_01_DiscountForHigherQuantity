03335cd279ff4217a9c985f2b249fbb04ac6b89f
Nguyen Dai Truong Thanh <36881258+daitng17@users.noreply.github.com>
2021-03-03 00:18:39 +0900

    test cases

--- pcal/priset.py
+++ pcal/priset.py
@@ -34,19 +34,23 @@ engine1.discountprice(0.6)
 print(engine1.price())
 print(engine1.netfactor())
 
-# test with a list of part
+# test with a list of parts
 
-pricelist = (1000, 2000, 500)
+pricelist = [1000, 2000, 500]
 
 for e in pricelist:
     pri = PartsPricing(e)
     pri.discountprice(0.7)
     print(pri.price())
 
+# test with a dict of different parts and prices
+
+pricedict = {'en1' : [1000, 0.6], 'en2' : [2000, 0.5], 'en3' : [500, 0.7]}
+
+for k, v in pricedict.items():
+    pri = PartsPricing(v[0])
+    pri.discountprice(v[1])
+    print(k + ':' + str(pri.price()))
 
 
-'''LP = 2000
-    D = 0.6
-    F = 0.4
-   DP = 800'''
 
